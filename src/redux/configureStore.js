b4562f7429c9e3f105e756027262e7f2f47b8158
SaqibArsalan <saqibarsalan1997@gmail.com>
2021-02-06 23:23:12 +0500

    First commit

---
+++ src/redux/configureStore.js
@@ -0,0 +1,28 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Dishes } from './dishes';
+import { Leaders } from './leaders';
+import { Comments } from './comments';
+import { Promotions } from './promotions';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { createForms } from 'react-redux-form';
+import { initialFeedback } from './forms';
+
+
+export const ConfigureStore = () => {
+    const store = createStore(
+       combineReducers({
+           dishes: Dishes,
+           comments: Comments,
+           promotions: Promotions,
+           leaders: Leaders,
+           ...createForms({
+               feedback: initialFeedback
+           })
+       }),
+       applyMiddleware(thunk, logger)
+
+    );
+
+    return store;
+}
